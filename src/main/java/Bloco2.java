675040394453024cd8fadfaf2388be48926ddfc8
Mariana Pereira <marianabsspereira@gmail.com>
2021-10-22 11:47:15 +0100

    Exercicios bloco 2 todos

--- src/main/java/Bloco2.java
+++ src/main/java/Bloco2.java
@@ -21,6 +21,11 @@ public class Bloco2 {
         } else {
             System.out.println("Aluno nao aprovado");
         }
+        //exercicio 11, bloco 2: c). Tb não funciona com alterações feitas em e)
+        /*System.out.println("Introduzir aprovados ]0,1[");
+        double aprovados = sc.nextDouble();
+        System.out.println(Bloco2.classificaTurma(aprovados));*/
+
     }
     //exercicio 1, bloco 2
     public static double mediaPonderada (double nota1, double nota2, double nota3, double peso1,
@@ -159,5 +164,270 @@ public class Bloco2 {
             msg = num1 + " e " + num2 + " não são múltiplos";
         }
         return msg;
+    }
+    //exercicio 9, bloco 2
+    public static boolean crescente (int numero){
+        int digito3 = numero % 10;
+        int digito2 = (numero / 10) % 10;
+        int digito1 = (numero / 100);
+        boolean resultado;
+
+        if (numero<100 || numero>999){
+            resultado = false; //se numero nao tem 3 digitos devolve false
+        }
+        if (digito3>digito2 && digito3>digito1){
+            if (digito2>digito1){
+                resultado = true;
+            } else {
+                resultado = false;
+            }
+        } else {
+            resultado = false;
+        }
+        return resultado;
+    }
+    //exercicio 10, bloco 2
+    public static int descontoPreco (double preco){
+        int desconto;
+        if (preco > 200){
+            desconto = 60;
+        } else if (preco > 100 && preco <= 200) {
+            desconto = 40;
+        } else if (preco > 50 && preco <= 100) {
+            desconto = 30;
+        } else {
+            desconto = 20;
+        }
+        return desconto;
+    }
+    //exercicio 11, bloco 2: a) classifica a turma com base na % de alunos aprovados
+   public static String classificaTurma (double aprovados, double lim1, double lim2, double lim3, double lim4){
+        String resultado;
+
+        if (aprovados<0 || aprovados>1){
+            resultado = "Valor Inválido";
+        } else if (aprovados < lim1){ // b) (aprovados < 0.2)
+            resultado = "Turma Má";
+        } else if (aprovados < lim2){ // b) (aprovados < 0.5)
+            resultado = "Turma Fraca";
+        } else if (aprovados < lim3){ // b) (aprovados < 0.7)
+            resultado = "Turma Razoável";
+        } else if (aprovados < lim4){ // b) (aprovados < 0.9)
+            resultado = "Turma Boa";
+        } else {
+            resultado = "Turma Excelente";
+        }
+
+        return resultado;
+   }
+   //exercicio 12, bloco 2
+    public static String indicePoluicao (double indiceReal) {
+        String msg;
+
+        if (indiceReal < 0){
+            msg = "Valor inválido";
+        } else if (indiceReal >= 0 && indiceReal <= 0.3){
+            msg = "Valor OK";
+        } else if (indiceReal > 0.3 && indiceReal <= 0.4 ){
+            msg = "Grupo empresas 1 suspendem actividade";
+        } else if (indiceReal > 0.4 && indiceReal <= 0.5){
+            msg = "Grupo empresas 1 e 2 suspendem actividade";
+        } else {
+            msg = "Grupo empresas 1, 2 e 3 suspendem actividade";
+        }
+
+        return msg;
+    }
+    //exercicio 13, bloco 2
+    public static double horasJardim (double area, int nArvores, int nArbustos){
+        double nHoras;
+        int gramaTempo = 300;
+        int arvoresTempo = 600;
+        int arbustosTempo = 400;
+
+        if (area < 0 || nArvores < 0 || nArbustos < 0){
+            nHoras = Double.NaN;
+        }
+        nHoras = (area * gramaTempo + arvoresTempo * nArvores + arbustosTempo * nArbustos) / 60 / 60;
+
+        return nHoras;
+    }
+    public static double custoJardim (double area, int nArvores, int nArbustos){
+        double custo;
+        int gramaCusto = 10;
+        int arvoresCusto = 20;
+        int arbustosCusto = 15;
+        int custoHora = 10;
+
+        if (area < 0 || nArvores < 0 || nArbustos < 0){
+           custo = Double.NaN;
+        }
+        custo = (area * gramaCusto + arvoresCusto * nArvores + arbustosCusto * nArbustos) +
+                Bloco2.horasJardim(area,nArvores,nArbustos) * custoHora ;
+
+        return custo;
+    }
+    //exercicio 14, bloco 2
+    public static double mediaKmDistancia (double milhasDia1,double milhasDia2,double milhasDia3,double milhasDia4,
+                                           double milhasDia5)    {
+        double media;
+
+        if (milhasDia1<0 || milhasDia2<0 || milhasDia3<0 || milhasDia4<0 || milhasDia5<0){
+            media = Double.NaN;
+        }
+
+        media = ((milhasDia1 + milhasDia2 + milhasDia3 + milhasDia4 + milhasDia5)/5) * 1.609;
+
+        return media;
+    }
+    //exercicio 15, bloco 2
+    public static String classificaTriangulo (double lado1, double lado2, double lado3){
+        String resultado;
+        if (lado1 < 0 || lado2 < 0 || lado3 < 0){
+            resultado = "Medidas inválidas";
+        } else if (lado1 >= lado2 + lado3 || lado2 >= lado3 + lado1 || lado3 >= lado2 + lado1 ){
+            resultado = "Medidas inválidas";
+        } else {
+            if (lado1 == lado2 && lado1 == lado3){
+                resultado = "Equilátero";
+            } else if(lado1 != lado2 && lado1 != lado3 && lado2 != lado3){
+                resultado = "Escaleno";
+            } else {
+                resultado = "Isósceles";
+            }
+        }
+
+        return resultado;
+    }
+    //exercicio 16, bloco 2
+    public static String classificaTrianguloAngulos (double angulo1, double angulo2, double angulo3){
+        String resultado;
+        if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+            resultado = "Medidas inválidas";
+        } else if (angulo1 + angulo2 + angulo3 != 180){
+            resultado = "Medidas inválidas";
+        } else {
+            if (angulo1 == 90 || angulo2 == 90 || angulo3 == 90){
+                resultado = "Retângulo";
+            } else if(angulo1 > 90 || angulo2 > 90 || angulo3 > 90){
+                resultado = "Obtusângulo";
+            } else {
+                resultado = "Acutângulo";
+            }
+        }
+
+        return resultado;
+    }
+    //exercicio 17, bloco 2
+    public static String tempoChegadaCP (int horaPartida, int minutosPartida, int duracaoHoras, int duracaoMinutos){
+        int minutosChegada;
+        int horasChegada = 0;
+        String diaChegada;
+        String msg;
+
+        minutosChegada = minutosPartida + duracaoMinutos;
+        if (minutosChegada >= 60){
+            horasChegada = (int) (minutosChegada / 60);
+            minutosChegada = (minutosPartida + duracaoMinutos) % 60;
+        }
+        horasChegada =  horasChegada + horaPartida + duracaoHoras;
+
+        if (horasChegada >= 24){
+            diaChegada = "Dia seguinte";
+            horasChegada = horasChegada - 24;
+        } else {
+            diaChegada = "Próprio dia";
+        }
+
+        msg = horasChegada + ":" + minutosChegada + ", Data: " + diaChegada;
+
+        return msg;
+    }
+    //exercicio 18, bloco 2 // assumindo que no tempo de inicio, horas <=24 e minutos e segundos < 60
+    public static String processamentoTarefa (int horasInicio, int minutosInicio, int segundosInicio,
+                                              int duracaoSegundos){
+        String msg;
+        int horasFim;
+        int minutosFim;
+        int segundosFim;
+        int inicioEmSegundos = (horasInicio * 60 * 60) + (minutosInicio * 60) + segundosInicio;
+        int fimEmSegundos = inicioEmSegundos + duracaoSegundos;
+
+        horasFim = (fimEmSegundos / (60*60));
+        minutosFim = (fimEmSegundos % (60*60)) / 60;
+        segundosFim = fimEmSegundos - horasFim*60*60 - minutosFim*60;
+
+        if (horasFim>=24) {
+            horasFim = horasFim - 24;
+        }
+
+        msg = horasFim + ":" + minutosFim + ":" + segundosFim;
+
+        return msg;
+    }
+    //exercicio 19, bloco 2
+    public static double salarioSemanal (double horasSemanais){
+        double horasSemanaisNormais = 36; //double so pq pode mudar ex: 36,5
+        double valorHoraNormal = 7.5;
+        double valorHoraExtra1 = 10;
+        int valorLimite1 = 5;
+        double valorHoraExtra2 = 15;
+        double resultado;
+
+        if (horasSemanais <= 0){
+           return resultado = 0; // return para terminar a funçao caso valor seja 0 ou negativo
+        }
+
+        if (horasSemanais <= horasSemanaisNormais) {
+            resultado =  horasSemanais * valorHoraNormal;
+        } else if (horasSemanais > horasSemanaisNormais && horasSemanais <= (horasSemanaisNormais + valorLimite1)){
+            resultado = horasSemanaisNormais * valorHoraNormal + (horasSemanais - horasSemanaisNormais)*valorHoraExtra1;
+        } else {
+            resultado = horasSemanaisNormais * valorHoraNormal + valorLimite1 * valorHoraExtra1
+            + (horasSemanais - horasSemanaisNormais - valorLimite1) * valorHoraExtra2;
+        }
+
+        return resultado;
+    }
+    //exercicio 20, bloco 2 // Não tem protecção para valores inválidos
+    public static double valorAluguer (int diaSemana, String tipoKit, double distanciaKm, boolean feriadoSimOuNao){
+        double custoKitA = 30;
+        double custoKitB = 50;
+        double custoKitC = 100;
+        double custoKitAExtra = 40;
+        double custoKitBExtra = 70;
+        double custoKitCExtra = 140;
+        double precoKm = 2;
+        double custoTotal;
+
+        if (feriadoSimOuNao == false){
+            if (diaSemana==2 || diaSemana==3 || diaSemana==4 || diaSemana==5 || diaSemana==6){
+                if (tipoKit == "A"){
+                    custoTotal = custoKitA + distanciaKm * precoKm;
+                } else if (tipoKit == "B"){
+                    custoTotal = custoKitB + distanciaKm * precoKm;
+                } else {
+                    custoTotal = custoKitC + distanciaKm * precoKm;
+                }
+            } else {
+                if (tipoKit == "A"){
+                    custoTotal = custoKitAExtra + distanciaKm * precoKm;
+                } else if (tipoKit == "B"){
+                    custoTotal = custoKitBExtra + distanciaKm * precoKm;
+                } else {
+                    custoTotal = custoKitCExtra + distanciaKm * precoKm;
+                }
+            }
+        } else {
+            if (tipoKit == "A"){
+                custoTotal = custoKitAExtra + distanciaKm * precoKm;
+            } else if (tipoKit == "B"){
+                custoTotal = custoKitBExtra + distanciaKm * precoKm;
+            } else {
+                custoTotal = custoKitCExtra + distanciaKm * precoKm;
+            }
+        }
+
+        return custoTotal;
     }
 }
--- src/test/java/Bloco2Test.java
+++ src/test/java/Bloco2Test.java
@@ -198,4 +198,371 @@ class Bloco2Test {
         String result = Bloco2.multiplos(3,7);
         assertEquals(expected, result);
     }
+    // testes ex 9
+    @Test
+    void crescenteTeste1() {
+        boolean expected = true;
+        boolean result = Bloco2.crescente(345);
+        assertEquals(expected, result);
+    }
+    @Test
+    void crescenteTeste2() {
+        boolean expected = false;
+        boolean result = Bloco2.crescente(546);
+        assertEquals(expected, result);
+    }
+    @Test
+    void crescenteTeste3() {
+        boolean expected = false;
+        boolean result = Bloco2.crescente(231);
+        assertEquals(expected, result);
+    }
+    @Test
+    void crescenteTeste4() {
+        boolean expected = false;
+        boolean result = Bloco2.crescente(759);
+        assertEquals(expected, result);
+    }
+    @Test
+    void crescenteTeste5() {
+        boolean expected = false;
+        boolean result = Bloco2.crescente(8975);
+        assertEquals(expected, result);
+    }
+    // testes ex 10
+    @Test
+    void descontoPrecoTeste1() {
+        int expected = 60;
+        int result = Bloco2.descontoPreco(200.1);
+        assertEquals(expected, result, 0.1);
+    }
+    @Test
+    void descontoPrecoTeste2() {
+        int expected = 40;
+        int result = Bloco2.descontoPreco(200);
+        assertEquals(expected, result, 0.1);
+    }
+    @Test
+    void descontoPrecoTeste3() {
+        int expected = 30;
+        int result = Bloco2.descontoPreco(100);
+        assertEquals(expected, result, 0.1);
+    }
+    @Test
+    void descontoPrecoTeste4() {
+        int expected = 20;
+        int result = Bloco2.descontoPreco(50);
+        assertEquals(expected, result, 0.1);
+    }
+    @Test
+    void descontoPrecoTeste5() {
+        int expected = 40;
+        int result = Bloco2.descontoPreco(100.1);
+        assertEquals(expected, result, 0.1);
+    }
+    // testes ex 11 : d). Não funcionam com as alterações feita em e)
+   /* @Test
+    void classificaTurmaTeste1() {
+        String expected = "Turma Má";
+        String result = Bloco2.classificaTurma(0.1);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste2() {
+        String expected = "Turma Fraca";
+        String result = Bloco2.classificaTurma(0.2);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste3() {
+        String expected = "Turma Razoável";
+        String result = Bloco2.classificaTurma(0.6);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste4() {
+        String expected = "Turma Boa";
+        String result = Bloco2.classificaTurma(0.8);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste5() {
+        String expected = "Turma Excelente";
+        String result = Bloco2.classificaTurma(0.95);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste6() {
+        String expected = "Valor Inválido";
+        String result = Bloco2.classificaTurma(1.1);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTurmaTeste7() {
+        String expected = "Valor Inválido";
+        String result = Bloco2.classificaTurma(-0.1);
+        assertEquals(expected, result);
+    }*/
+    // testes ex 11 : e)
+    @Test
+    void classificaTurmaTeste1() {
+        String expected = "Turma Fraca";
+        String result = Bloco2.classificaTurma(0.4,0.2,0.5,0.7,0.9);
+        assertEquals(expected, result);
+    }
+    // testes ex 12
+    @Test
+    void indicePoluicaoTeste1() {
+        String expected = "Valor inválido";
+        String result = Bloco2.indicePoluicao(-0.1);
+        assertEquals(expected, result);
+    }
+    @Test
+    void indicePoluicaoTeste2() {
+        String expected = "Valor OK";
+        String result = Bloco2.indicePoluicao(0.3);
+        assertEquals(expected, result);
+    }
+    @Test
+    void indicePoluicaoTeste3() {
+        String expected = "Grupo empresas 1 suspendem actividade";
+        String result = Bloco2.indicePoluicao(0.4);
+        assertEquals(expected, result);
+    }
+    @Test
+    void indicePoluicaoTeste4() {
+        String expected = "Grupo empresas 1 e 2 suspendem actividade";
+        String result = Bloco2.indicePoluicao(0.5);
+        assertEquals(expected, result);
+    }
+    @Test
+    void indicePoluicaoTeste5() {
+        String expected = "Grupo empresas 1, 2 e 3 suspendem actividade";
+        String result = Bloco2.indicePoluicao(0.6);
+        assertEquals(expected, result);
+    }
+    // testes ex 13
+    @Test
+    void horasJardimTeste1() {
+        double expected = 10.6;
+        double result = Bloco2.horasJardim(100,10,5);
+        assertEquals(expected, result,0.1);
+    }
+    @Test
+    void custoJardimTeste1() {
+        double expected = 1380.6;
+        double result = Bloco2.custoJardim(100,10,5);
+        assertEquals(expected, result,0.1);
+    }
+    // testes ex 14
+    @Test
+    void mediaKmDistanciaTeste1() {
+        double expected = 3.2;
+        double result = Bloco2.mediaKmDistancia(2,2,2,2,2);
+        assertEquals(expected, result,0.1);
+    }
+    // testes ex 15
+    @Test
+    void classificaTrianguloTeste1() {
+        String expected = "Medidas inválidas";
+        String result = Bloco2.classificaTriangulo(30,5,5);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloTeste2() {
+        String expected = "Medidas inválidas";
+        String result = Bloco2.classificaTriangulo(5,56,2);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloTeste3() {
+        String expected = "Medidas inválidas";
+        String result = Bloco2.classificaTriangulo(5,10,16);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloTeste4() {
+        String expected = "Escaleno";
+        String result = Bloco2.classificaTriangulo(5,10,6);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloTeste5() {
+        String expected = "Equilátero";
+        String result = Bloco2.classificaTriangulo(5,5,5);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloTeste6() {
+        String expected = "Isósceles";
+        String result = Bloco2.classificaTriangulo(5,5,2);
+        assertEquals(expected, result);
+    }
+    // testes ex 16
+    @Test
+    void classificaTrianguloAngulosTeste1() {
+        String expected = "Medidas inválidas";
+        String result = Bloco2.classificaTrianguloAngulos(90,90,30);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloAngulosTeste2() {
+        String expected = "Medidas inválidas";
+        String result = Bloco2.classificaTrianguloAngulos(90,62,30);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloAngulosTeste3() {
+        String expected = "Retângulo";
+        String result = Bloco2.classificaTrianguloAngulos(90,45,45);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloAngulosTeste4() {
+        String expected = "Obtusângulo";
+        String result = Bloco2.classificaTrianguloAngulos(100,40,40);
+        assertEquals(expected, result);
+    }
+    @Test
+    void classificaTrianguloAngulosTeste5() {
+        String expected = "Acutângulo";
+        String result = Bloco2.classificaTrianguloAngulos(60,60,60);
+        assertEquals(expected, result);
+    }
+    // testes ex 17
+    @Test
+    void tempoChegadaCPTeste1() {
+        String expected = "18:15, Data: Próprio dia"; // msg = horasChegada + ":" + minutosChegada + ", Data: " + diaChegada;
+        String result = Bloco2.tempoChegadaCP(17,05,1,10);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste2() {
+        String expected = "19:49, Data: Próprio dia";
+        String result = Bloco2.tempoChegadaCP(17,59,1,50);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste3() {
+        String expected = "1:0, Data: Dia seguinte";
+        String result = Bloco2.tempoChegadaCP(20,0,5,0);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste4() {
+        String expected = "3:0, Data: Dia seguinte";
+        String result = Bloco2.tempoChegadaCP(22,0,5,0);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste5() {
+        String expected = "0:0, Data: Dia seguinte";
+        String result = Bloco2.tempoChegadaCP(20,0,4,0);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste6() {
+        String expected = "0:10, Data: Dia seguinte";
+        String result = Bloco2.tempoChegadaCP(20,10,4,0);
+        assertEquals(expected, result);
+    }
+    @Test
+    void tempoChegadaCPTeste7() {
+        String expected = "1:0, Data: Dia seguinte";
+        String result = Bloco2.tempoChegadaCP(20,60,4,0);
+        assertEquals(expected, result);
+    }
+    // testes ex 18
+    @Test
+    void processamentoTarefaTeste1() {
+        String expected = "8:15:5"; // msg = horasFim + ":" + minutosFim + ":" + segundosFim;
+        String result = Bloco2.processamentoTarefa(7,15,5,3600);
+        assertEquals(expected, result);
+    }
+    @Test
+    void processamentoTarefaTeste2() {
+        String expected = "10:17:59";
+        String result = Bloco2.processamentoTarefa(5,17,59,18000);
+        assertEquals(expected, result);
+    }
+    @Test
+    void processamentoTarefaTeste3() {
+        String expected = "1:59:59";
+        String result = Bloco2.processamentoTarefa(20,59,59,18000);
+        assertEquals(expected, result);
+    }
+    @Test
+    void processamentoTarefaTeste4() {
+        String expected = "21:59:59";
+        String result = Bloco2.processamentoTarefa(20,59,59,90000);
+        assertEquals(expected, result);
+    }
+    // testes ex 19
+    @Test
+    void salarioSemanalTeste1() {
+        double expected = 270;
+        double result = Bloco2.salarioSemanal(36);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void salarioSemanalTeste2() {
+        double expected = 280;
+        double result = Bloco2.salarioSemanal(37);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void salarioSemanalTeste3() {
+        double expected = 335;
+        double result = Bloco2.salarioSemanal(42);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void salarioSemanalTeste4() {
+        double expected = 380;
+        double result = Bloco2.salarioSemanal(45);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void salarioSemanalTeste5() {
+        double expected = 0;
+        double result = Bloco2.salarioSemanal(0);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void salarioSemanalTeste6() {
+        double expected = 0;
+        double result = Bloco2.salarioSemanal(-10);
+        assertEquals(expected, result,0.01);
+    }
+    // testes ex 20
+    @Test
+    void valorAluguerTeste1() {
+        double expected = 60;
+        double result = Bloco2.valorAluguer(1,"A",10,false);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void valorAluguerTeste2() {
+        double expected = 50;
+        double result = Bloco2.valorAluguer(2,"A",10,false);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void valorAluguerTeste3() {
+        double expected = 120;
+        double result = Bloco2.valorAluguer(2,"C",10,false);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void valorAluguerTeste4() {
+        double expected = 70;
+        double result = Bloco2.valorAluguer(2,"B",10,false);
+        assertEquals(expected, result,0.01);
+    }
+    @Test
+    void valorAluguerTeste5() {
+        double expected = 50;
+        double result = Bloco2.valorAluguer(2,"A",5,true);
+        assertEquals(expected, result,0.01);
+    }
 }
